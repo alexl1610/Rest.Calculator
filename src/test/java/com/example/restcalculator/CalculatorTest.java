5c99bd8512276d3461fa0dc7eb5fa42099165664
Alexander Lang <alexander.lang1610@gmail.com>
2022-06-15 16:03:13 +0200

    add tests

--- src/test/java/com/example/restcalculator/CalculatorTest.java
+++ src/test/java/com/example/restcalculator/CalculatorTest.java
@@ -23,4 +23,56 @@ class CalculatorTest {
         // is expected == result?
         assertEquals(expected, result);
     }
+    @Test
+    void subTest() {
+        int a = 10;
+        int b = 6;
+        int expected = 4;
+
+        int result = calculator.sub(a, b);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void multiplyTest() {
+        int a = 10;
+        int b = 6;
+        int expected = 60;
+
+        int result = calculator.multiply(a, b);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void divTestDividedZero() {
+        int a = 10;
+        int b = 0;
+
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(a, b);
+        });
+    }
+
+    @Test
+    void pwrTest() {
+        int a = 3;
+        int b = 2;
+        int expected = 9;
+
+        double result = calculator.pwr(a, b);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void absTest() {
+        int a = -1;
+        int expected = 1;
+
+        int result = calculator.abs(a);
+
+        assertEquals(expected, result);
+    }
 }
